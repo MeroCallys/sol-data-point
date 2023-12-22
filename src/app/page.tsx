5b431f8cd265c18df37d4867bde17a1abed4124b
MeroCallys <MeroCallys@gmail.com>
2023-12-22 15:30:34 -0700

    Version 0: starting file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div></div>;
+import OrdersCards from "@/components/orders-cards";
+import OrdersTable from "@/components/orders-table";
+import { fetchOrders, OrdersProps } from "@/data/placeholders";
+
+export default async function Home() {
+  const orders = await fetchOrders();
+
+  return (
+    <section className="w-full h-full p-5">
+      {/* <OrdersTable orders={orders} /> */}
+      <OrdersCards orders={orders} />
+    </section>
+  );
 }
